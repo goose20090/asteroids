c68b619aa044fce6b4de68ef95447433cde5e2df
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2025-01-26 19:48:50 +0000

    add delta time var and limit refresh rate to 60 fps

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock 
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +18,7 @@ def main():
         
         screen.fill(color=[0,0,0])
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
