b0f3c537c5564f7a052e0b872eb986afd29fe084
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2025-01-26 20:37:50 +0000

    add drawable and updatable groups, integrate into event loop

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ def main():
 
     player = Player(x, y)
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
